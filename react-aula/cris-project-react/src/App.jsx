78dc65fab0da010641dcdc9682c3a186c004aace
cristina <cdias07@gmail.com>
2023-11-15 14:33:53 -0300

    adicionado efeitos em campo de busca de pesquisa

--- react-aula/cris-project-react/src/App.jsx
+++ react-aula/cris-project-react/src/App.jsx
@@ -1,35 +1,45 @@
-// import MeuComponente from "./components/MeuComponente"
-// import MeuContador from "./components/MeuContador"
+import { useEffect, useState } from "react"
 
-import MinhaLista from "./components/MinhaLista"
-function App() {
+const minhalista = [
+  { id: '1', value: 'Fruta'},
+  { id: '2', value: 'Legume'},
+  { id: '3', value: 'Carne'},
+
+]
+
+export default function App() {
+
+  const[ produtos, setProdutos] = useState(minhalista);
+  const[ pesquisa, setPesquisa] = useState('');
+
+  useEffect(
+    () => {
+      if(pesquisa) {
+        const novaLista = minhalista.filter( (item) => {
+          return item.value.toLowerCase().includes(pesquisa.toLowerCase());
+        } );
+        setProdutos(novaLista);
+      } else {
+        setProdutos(minhalista);
+      }
+    }
+  ,[pesquisa])
 
   return (
     <div>
-      {/* <h1>Olá Mundo, React!</h1>
-      <MeuComponente />
-    
-      <MeuBotao conteudo='me clique' />
+      <h1>Efeitos Colaterais</h1>
+      <input 
+        value={pesquisa}
+        onChange = {(e) => setPesquisa(e.target.value)}
+        placeholder="pesquise aqui..." type="text" />
 
-      <MeuBotao conteudo='depois aqui'/>
-
-      <MeuBotao conteudo='e por fim aqui'/>
-
-      <MeuContador /> */}
-
-      <h1>Listas no React</h1>
-
-      <MinhaLista />
-
+        {produtos.map( (item) => {
+          return (
+            <div key={item.id}>
+              <h4> {item.value} </h4>
+            </div>
+          )
+        })} 
     </div>
   )
 }
-
-function MeuBotao(props) {
-  return (
-    <button> {props.conteudo} </button>
-  )
-}
-
-
-export default App
